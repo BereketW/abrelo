294a97451723e133d2a383a56db7e491c801eb8e
BereketW <bereketsodenoo@gmail.com>
2024-09-05 00:42:48 +0300

    Revert "pushed to github"
    This reverts commit 787dce035b39ea94259e0ddee94e905c8232d4bf.

--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import About from "@/components/About";
-import Header from "@/components/Header";
-import React from "react";
-export default function Page(){
-    return(
-        <><Header className="bg-hero shadow-md sticky top-0 text-white px-24"/>
-        <About /></>
-    )
-}
